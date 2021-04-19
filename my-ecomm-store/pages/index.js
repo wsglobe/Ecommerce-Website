102cd8c56c549a974ffd5421fb42b6b483bf9fa1
Walter Wang <wsglobe@gmail.com>
2021-04-19 16:30:29 -0500

    title des change

--- my-ecomm-store/pages/index.js
+++ my-ecomm-store/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          The best space jellyfish swag on the web!
+          The best space jellyfish swag in the universe!
         </p>
 
         <ul className={styles.grid}>
