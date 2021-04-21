c2547bb61a736daeb1a7063097324633957bbac9
Walter Wang <wsglobe@gmail.com>
2021-04-21 03:31:10 -0500

    hide footer

--- my-ecomm-store/pages/index.js
+++ my-ecomm-store/pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
         </ul>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -53,7 +53,7 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
